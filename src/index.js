1cac3c4962a53e6dbb4147d93d5f9647f2b73026
Jakub Argasiński <jakub@argasinski.eu>
2021-03-24 20:07:05 +0100

    Exercise 01 - the basics

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-const answer = prompt('Ile jest 7 * 3 ?');
-console.log(answer);
+// statements -- instrukcje
+// expressions -- wyrażenia
+
+function sayHelloAndGoodbye(name) {
+  console.log('Hi ' + name);
+  console.log('Goodbay ' + name);
+}
+
+
+function square(x) {
+  return x * x;
+}
+
+let a = 5;
+const fullName = 'Jakub Argasiński';
+
+a = 666;
+fullName = 'Janina Kowalska';
+
+
+console.log(square(2 + 3));
+
+sayHelloAndGoodbye('Piotr');
+sayHelloAndGoodbye('Waldek');
+sayHelloAndGoodbye('Aga');
+
