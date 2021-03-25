09b8ddde4caa8479bc2ea5fbfdfb9bf8262d645c
Jakub Argasiński <jakub@argasinski.eu>
2021-03-25 18:39:51 +0100

    Add prompt asking user for his/her name

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// statements -- instrukcje
-// expressions -- wyrażenia
-
-function sayHelloAndGoodbye(name) {
-  console.log('Hi ' + name);
-  console.log('Goodbay ' + name);
-}
-
-
-function square(x) {
-  return x * x;
-}
-
-let a = 5;
-const fullName = 'Jakub Argasiński';
-
-a = 666;
-fullName = 'Janina Kowalska';
-
-
-console.log(square(2 + 3));
-
-sayHelloAndGoodbye('Piotr');
-sayHelloAndGoodbye('Waldek');
-sayHelloAndGoodbye('Aga');
+const fullName = prompt('What is your name?');
+console.log(fullName);
 
