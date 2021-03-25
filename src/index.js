f6b1b632611b3ce1a673d6ebac40ed6921a59141
Peter <pgoraczko@hotmail.com>
2021-03-25 13:01:49 -0700

    Introduce isNaN() function.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const age = prompt('What is your age?');
-console.log(parseInt(age));
-
+const parsedAge = parseInt(age);
+console.log(parsedAge);
+if ( isNaN(parsedAge)){
+    console.log('ParsedAge is not a number');
+}
 console.log('In 25 years you will be ' + (parseInt(age) + 25) + ' years old');
 
