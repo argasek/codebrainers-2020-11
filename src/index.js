e231b91e5e9acea08b4f47cb0eaa8fcc20e90d20
Aga Kowalska <kowalska.a3@gmail.com>
2021-03-25 20:17:53 +0100

    Ask user for age instead of name. Parse age as integer. Perform simple calculations as result.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const fullName = prompt('What is your name?');
-console.log(fullName);
+const age = prompt('What is your age?');
+console.log(parseInt(age));
+
+console.log('In 25 years you will be ' + (parseInt(age) + 25) + ' years old');
 
