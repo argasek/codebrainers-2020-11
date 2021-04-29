d77cedab56287df5342c6dafc3bd58aaa95ed224
Aga Kowalska <kowalska.a3@gmail.com>
2021-04-29 10:50:51 -0700

    Introduced usage of setState() method.

--- src/App.js
+++ src/App.js
@@ -4,42 +4,58 @@ import StudentItem from './components/StudentItem';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      exampleStudent: 'Jarosław I wspaniały'
-    };
-  }
-
-  render () {
-
-    console.log(this.state.exampleStudent);
-
-    function onChange(event) {
-      // exampleStudent = event.target.value;
-      console.log('Zaszła zmiana!', event.target.value);
+    constructor(props) {
+        super(props);
+        this.state = {
+            exampleStudent: 'Jarosław I wspaniały'
+        };
     }
 
-    return (
-      <div style={{ backgroundColor: '#ddd', padding: '4rem' }}>
-        <StudentItem fullName={this.state.exampleStudent} />
-        <StudentItem fullName={'Basia'}/>
-        <form>
-          <input
-            value={this.state.exampleStudent}
-            type="text"
-            style={{ marginLeft: '1rem', padding: '0.5rem', border: '2px solid #00F', borderRadius: '4px', fontSize: '150%' }}
-            onChange={onChange}
-          />
-          <input
-            type="text"
-            style={{ margin: '1rem', padding: '0.5rem', border: '2px solid #F00', borderRadius: '4px', fontSize: '150%' }}
-            onChange={onChange}
-          />
-        </form>
-      </div>
-    );
-  }
+    onChange = (event) => {
+        console.log(this);
+        // this.state.exampleStudent = event.target.value;
+        this.setState({exampleStudent: event.target.value});
+        console.log('Zaszła zmiana!', this.state.exampleStudent);
+    }
+
+    render() {
+
+        console.log(this.state.exampleStudent);
+        console.log(this);
+
+
+        return (
+            <div style={{backgroundColor: '#ddd', padding: '4rem'}}>
+                <StudentItem fullName={this.state.exampleStudent}/>
+                <StudentItem fullName={'Basia'}/>
+                <form>
+                    <input
+                        value={this.state.exampleStudent}
+                        type="text"
+                        style={{
+                            marginLeft: '1rem',
+                            padding: '0.5rem',
+                            border: '2px solid #00F',
+                            borderRadius: '4px',
+                            fontSize: '150%'
+                        }}
+                        onChange={this.onChange}
+                    />
+                    <input
+                        type="text"
+                        style={{
+                            margin: '1rem',
+                            padding: '0.5rem',
+                            border: '2px solid #F00',
+                            borderRadius: '4px',
+                            fontSize: '150%'
+                        }}
+                        onChange={this.onChange}
+                    />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
