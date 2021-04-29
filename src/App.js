e225abfb017d3def9728a4fd14ac3a64ea1a38f5
Jakub Argasiński <jakub@argasinski.eu>
2021-04-29 19:18:05 +0200

    Exercise 12 - Introduction to React - state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import './App.css';
-import StudentList from './components/StudentList';
-import {otherStudents, students} from './models/Students';
+import StudentItem from './components/StudentItem';
 
+class App extends React.Component {
 
-function App () {
-  return (
-      <div style={{backgroundColor: '#ddd', padding: '1rem'}}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      exampleStudent: 'Jarosław I wspaniały'
+    };
+  }
 
-          <StudentList students={students} />
-          <StudentList students={otherStudents}/>
+  render () {
+
+    console.log(this.state.exampleStudent);
+
+    function onChange(event) {
+      // exampleStudent = event.target.value;
+      console.log('Zaszła zmiana!', event.target.value);
+    }
+
+    return (
+      <div style={{ backgroundColor: '#ddd', padding: '4rem' }}>
+        <StudentItem fullName={this.state.exampleStudent} />
+        <StudentItem fullName={'Basia'}/>
+        <form>
+          <input
+            value={this.state.exampleStudent}
+            type="text"
+            style={{ marginLeft: '1rem', padding: '0.5rem', border: '2px solid #00F', borderRadius: '4px', fontSize: '150%' }}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            style={{ margin: '1rem', padding: '0.5rem', border: '2px solid #F00', borderRadius: '4px', fontSize: '150%' }}
+            onChange={onChange}
+          />
+        </form>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
