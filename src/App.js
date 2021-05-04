90356501f9908815419ac104d63b9be2d0356f53
Peter <pgoraczko@hotmail.com>
2021-05-04 10:09:48 -0700

    Create SetFullName method

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ class App extends React.Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      fullName: ''
+    };
   }
+  setFullName () {
+
+  };
 
   render () {
 
     return (
       <div style={{ backgroundColor: '#ddd', padding: '4rem' }}>
-
+        <input type="text" />
       </div>
     );
   }
