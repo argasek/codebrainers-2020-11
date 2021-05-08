a6f8e261ce2935ea69ecea621b49b456a3efa759
Peter <pgoraczko@hotmail.com>
2021-05-08 07:44:52 -0700

    Added Delete rooms button

--- src/components/rooms/Room.jsx
+++ src/components/rooms/Room.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {exposures, humidities, temperatures, appearances} from "components/plants/PlantRow";
+import  {Button} from 'reactstrap';
 
 
 class Room extends React.PureComponent {
@@ -14,6 +15,7 @@ class Room extends React.PureComponent {
         <td>{ humidities[this.props.room.humidity] }</td>
         <td>{ temperatures[this.props.room.temperature] }</td>
         <td>{ appearances[this.props.room.draft] }</td>
+        <td> <Button onClick={()=> this.props.removeRoom(this.props.index)}> Remove Room </Button> </td>
       </tr>
     )
   }
--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, Table } from 'reactstrap';
+import {Card, CardBody, Table, Button} from 'reactstrap';
 import axios from 'axios';
 import Room from 'components/rooms/Room';
 import InProgress from 'components/shared/InProgress';
@@ -7,7 +7,7 @@ import InProgress from 'components/shared/InProgress';
 const ROOMS_FETCH_DELAY = 250;
 
 class Rooms extends React.PureComponent {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       rooms: [],
@@ -18,69 +18,69 @@ class Rooms extends React.PureComponent {
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetchRooms();
   }
 
-  fetchRooms () {
+  fetchRooms() {
     const requestUrl = 'http://gentle-tor-07382.herokuapp.com/rooms/';
-    this.setState({ inProgress: true });
+    this.setState({inProgress: true});
 
     return this.props.delayFetch(ROOMS_FETCH_DELAY, (resolve, reject) => {
       const promise = axios.get(requestUrl);
 
       promise
-        .then((response) => {
-          const data = response.data;
-          console.log(data);
-          const rooms = data.map((item) => {
-            const {
-              id,
-              name,
-              exposure,
-              temperature,
-              humidity,
-              draft,
-            } = item;
-            return {
-              id,
-              name,
-              exposure,
-              temperature,
-              humidity,
-              draft,
-            };
-          });
-          const successRooms = true;
-          this.setState({ rooms, successRooms });
-          resolve();
-        })
-        .catch((error) => {
-          this.setState({ successRooms: false });
-          reject();
-        })
-        .finally(() => {
-          this.setState({ inProgress: false });
-        });
+              .then((response) => {
+                const data = response.data;
+                console.log(data);
+                const rooms = data.map((item) => {
+                  const {
+                    id,
+                    name,
+                    exposure,
+                    temperature,
+                    humidity,
+                    draft,
+                  } = item;
+                  return {
+                    id,
+                    name,
+                    exposure,
+                    temperature,
+                    humidity,
+                    draft,
+                  };
+                });
+                const successRooms = true;
+                this.setState({rooms, successRooms});
+                resolve();
+              })
+              .catch((error) => {
+                this.setState({successRooms: false});
+                reject();
+              })
+              .finally(() => {
+                this.setState({inProgress: false});
+              });
     });
   }
 
   sortByExposure = () => {
     let numberOfExposureClicks = this.state.numberOfExposureClicks;
     const rooms = this.state.rooms;
-    const sortedRooms = [ ...rooms ].sort((a, b) => {
-      const exposureMapping = [ 'dark', 'shade', 'partsun', 'fullsun' ];
+    const sortedRooms = [...rooms].sort((a, b) => {
+      const exposureMapping = ['dark', 'shade', 'partsun', 'fullsun'];
       const exposureAindex = exposureMapping.indexOf(a.exposure);
       const exposureBindex = exposureMapping.indexOf(b.exposure);
       return (exposureAindex - exposureBindex) * (numberOfExposureClicks % 2 === 0 ? 1 : -1);
     });
-    this.setState({ rooms: sortedRooms, numberOfExposureClicks: ++numberOfExposureClicks });
+    this.setState({rooms: sortedRooms, numberOfExposureClicks: ++numberOfExposureClicks});
   };
 
   sortByName = () => {
     let numberOfNameClicks = this.state.numberOfNameClicks;
 
-    const sortedRooms = [ ...this.state.rooms ].sort((a, b) => {
+    const sortedRooms = [...this.state.rooms].sort((a, b) => {
 
       if (a.name > b.name) {
         return (numberOfNameClicks % 2 === 0 ? 1 : -1);
@@ -94,47 +94,61 @@ class Rooms extends React.PureComponent {
 
     });
 
-    this.setState({ rooms: sortedRooms, numberOfNameClicks: ++numberOfNameClicks });
+    this.setState({rooms: sortedRooms, numberOfNameClicks: ++numberOfNameClicks});
 
   };
 
-  render () {
-    const { rooms, successRooms, inProgress } = this.state;
+  createRoom = () => {
+    const rooms = [...this.state.rooms];
+    rooms.push({name: 'terrace', id: 0, exposure: 'fullsun', temperature: 'medium', humidity: 'medium', draft: false});
+    this.setState({rooms: rooms});
+  }
+
+  removeRoom = (index) => {
+    const rooms = [...this.state.rooms];
+   rooms.splice(index, 1);
+   this.setState({rooms:rooms});
+  }
+
+  render() {
+    const {rooms, successRooms, inProgress} = this.state;
 
     return (
-      <Card className="mb-4">
-        <CardBody>
-          <InProgress inProgress={inProgress}/>
-          {
-            successRooms === false &&
-            <p>Unable to fetch rooms.</p>
-          }
-          {
-            successRooms && (
-              <>
-                <Table>
-                  <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th onClick={this.sortByName}>Name</th>
-                    <th onClick={this.sortByExposure}>Exposure</th>
-                    <th>Humidity</th>
-                    <th>Temperature</th>
-                    <th>Draft</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  {
-                    rooms.map((room, index, arr) => (
-                            <Room room={room} key={index} index={index}/>)
-                    )
-                  }
-                  </tbody>
-                </Table>
-              </>
-            )}
-        </CardBody>
-      </Card>
+            <Card className="mb-4">
+              <CardBody>
+                <InProgress inProgress={inProgress}/>
+                {
+                  successRooms === false &&
+                  <p>Unable to fetch rooms.</p>
+                }
+                {
+                  successRooms && (
+                          <>
+                            <Table>
+                              <thead>
+                              <tr>
+                                <th>Id</th>
+                                <th onClick={this.sortByName}>Name</th>
+                                <th onClick={this.sortByExposure}>Exposure</th>
+                                <th>Humidity</th>
+                                <th>Temperature</th>
+                                <th>Draft</th>
+                                <th>Delete </th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              {
+                                rooms.map((room, index, arr) => (
+                                        <Room room={room} key={index} index={index} removeRoom={this.removeRoom}/>)
+                                )
+                              }
+                              </tbody>
+                            </Table>
+                          </>
+                  )}
+                <Button onClick={this.createRoom}> Add Room </Button>
+              </CardBody>
+            </Card>
     );
   }
 }
